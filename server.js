a0db5be9b506b348b01f6d0e06f946acc3df93da
Skertelfour <Skertelfour@gmail.com>
2025-04-01 11:59:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "kaodimnaka@gmail.com";
-//const pass = "phffbjesoudfutkq";
+const pass = "phffbjesoudfutkq";
 //skertel 30 feb
 
 // Middleware
